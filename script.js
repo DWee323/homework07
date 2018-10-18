b600fc45c39121e57d6816086cec90fac1fb2de7
Diane Wee <hwee1@id.iit.edu>
2018-10-18 10:51:20 -0500

    add function as described in prompt

--- script.js
+++ script.js
@@ -1,10 +1,50 @@
+//The final file should just the functions, no console.log calls or function calls.
+
+//The following tasks do not need to be done in order. I will review the final file.
+
+//For all functions, be sure to return a string "Error" 
+//if any of the parameters 
+//are missing, 
+//are not numbers, 
+//or have other mathmatical errors.
+
+//Take time and be efficient with your code.
+
+//If you don't remember some of this math, use google to refresh your mind.
+
 // for FUNCTIONS: INCLUDE {} AND 'returns' even in ARROW FUNCTIONS
 
 //ADD
-const add = (num1, num2) => {
+/*
+const add2 = (num1, num2) => {
     return num1 + num2;
 };
 console.log(add(7, 22));
+*/
+
+//Try ADD again
+const add = (num1, num2) => {
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        let ans = num1 + num2;
+        ans = ans.toFixed(2);
+        ans = parseFloat(ans);
+        return ans;        
+    } 
+    return "Error!";
+}
+
+console.log(add(7, 22),
+add(1, 2),  // returns 3
+add(0.1, 0.2),  // returns 0.3
+add(999, 1019),  // returns 2018
+add(),  // return "Error"
+add("Hello"),  // return "Error");
+);
+
+
+
+console.log(add(7, 22));
+
 
 //SUBRTRACT
 const subtract = (num1, num2) => {
@@ -44,10 +84,16 @@ const cube = (num) => {
     return answer;
 };
 
+
+
+
+
 //MODULO 
+/*
 const modulo = (dividend, divisor) => {
     return dividend % divisor;
 }
+*/
 
 //SQUARE ROOT: n^(1/2)
 //power function, other functions within functions
@@ -69,16 +115,15 @@ const power = (base, exponent) => {
 
 //X ROOT: n^(1/x)
 
-console.log(
-    add(7, 22),
-    subtract(7, 22),
+/*console.log( add(7, 22) );
+        subtract(7, 22),
     multiply(3, 8),
     divide(69, 2),
     power(2, 5),
     square(4),
     cube(3),
-    modulo(36, 11),
+    modulo(36, 11))
+    */
     //square_root(),
     //cube_root(),
     //x_root(, ),
-    );
